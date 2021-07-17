98746fa5106ca9d571cf554af620a6c98f4327e4
xClaudiaArias <xclaudiaarias@gmail.com>
2021-07-16 23:06:05 -0400

    updated files

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+const images = [];
+
+// create images element with document.createElement 
+// set its attributes 'src, alt, etc..'
+// insert it on the container
+
+// for level 1: generate 6 images, with 3 unique ones 
+// level 2: generate 10 images  with 5 unique ones
+// level 3:  generate 14 images with 7 unique ones
+

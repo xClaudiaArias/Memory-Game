def9ce91393656be23cd7a0412af7173a5ab88b5
xClaudiaArias <xclaudiaarias@gmail.com>
2021-07-22 00:16:35 -0400

    ..

--- js/scripts.js
+++ js/scripts.js
@@ -9,14 +9,47 @@ const  container = $("#container");
 // for level 1: generate 6 images, with 3 unique ones 
 // select 3 random images
 let numOfImages = 7;
+let imgArr = [];
+let imgArr2 = [];
+
 
 for (let i = 0; i < 3; i++){
     let randomIndex = Math.floor(Math.random() * numOfImages)
-    container.append("<img src='./Images/" + images[randomIndex] + ".jpg'>").attr("src", `./Images/{img}.jpg`)
-    images.splice(randomIndex, 1);
+    imgArr.push(images[randomIndex])
+    imgArr2.push(images[randomIndex])
+
+    // container.append("<img src='./Images/" + images[randomIndex] + ".jpg'>")
+    images.splice(randomIndex, 1)
     numOfImages -= 1;
     randomIndex = Math.floor(Math.random() * numOfImages)
+
 }
+
+let combinedArr = imgArr.concat(imgArr2)
+let lenCombArr = 6
+
+let randomizedArr = []
+
+for (const [i,img] of combinedArr.entries()){
+    console.log(i, img, " :i, img")
+    let rando =  Math.floor(Math.random() * lenCombArr)
+
+    container.append("<img src='./Images/" + combinedArr[rando] + ".jpg'>")
+    randomizedArr.push(combinedArr[i])
+
+    combinedArr.splice(rando, 1)
+    lenCombArr--
+    container.append("<img src='./Images/" + combinedArr[rando] + ".jpg'>")
+}
+
+
+
+
+
+console.log(imgArr, imgArr2, combinedArr, randomizedArr, " :imgArr, imgArr2, combinedArr, randomizedArr")
+
+
+
 
 // LEVEL 1 TO DO: 
 // -- add ID to images

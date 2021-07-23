bca1ef5f2c5e6e8a930b7cdd6a7854cfc0d33d6e
xClaudiaArias <xclaudiaarias@gmail.com>
2021-07-22 20:39:59 -0400

    randomized images

--- js/scripts.js
+++ js/scripts.js
@@ -1,83 +1,84 @@
 const images = ["one", "two", "three", "four", "five", "six", "seven"];
-const  container = $("#container");
+const container = $("#container");
 
 
-// create images element with document.createElement 
-// set its attributes 'src, alt, etc..'
-// insert it on the container
-
-// for level 1: generate 6 images, with 3 unique ones 
-// select 3 random images
-let numOfImages = 7;
-let imgArr = [];
-let imgArr2 = [];
+// selects random images 
 
 
-for (let i = 0; i < 3; i++){
-    let randomIndex = Math.floor(Math.random() * numOfImages)
-    imgArr.push(images[randomIndex])
-    imgArr2.push(images[randomIndex])
+const game = () => {
 
-    // container.append("<img src='./Images/" + images[randomIndex] + ".jpg'>")
-    images.splice(randomIndex, 1)
-    numOfImages -= 1;
-    randomIndex = Math.floor(Math.random() * numOfImages)
+    // get number of images 
+    let numOfImages = images.length; 
+    let imgArr = [];
+    let imgArr2 = [];
+    
+    for (let i = 0; i < 3; i++){
+        let randomIndex = Math.floor(Math.random() * numOfImages)
+        imgArr.push(images[randomIndex])
+        imgArr2.push(images[randomIndex])
+    
+        images.splice(randomIndex, 1)
+        numOfImages -= 1;
+        randomIndex = Math.floor(Math.random() * numOfImages)
+    }
+    
+    // #randomizes images  
+    let combinedArr = imgArr.concat(imgArr2)
+    let lenCombArr = combinedArr.length
+    let randomizedArr = []
+    
+    for (let i = 0; i < 6; i++){
+        let randomIndex = Math.floor(Math.random() * lenCombArr)
+        console.log(randomIndex, " :randomIndex")
+    
+        randomizedArr.push(combinedArr[randomIndex])
+        combinedArr.splice(randomIndex, 1)
+    
+        lenCombArr -= 1;
+    }
+    
+    // # displays images 
+    for (let [i, img] of randomizedArr.entries()){
+        console.log(i, img, " :i, img")
+        container.append(`<img id="card-${i + 1}" class="card" src="./Images/${img}.jpg">`)
+    }
+    
+    
+    
+    
+    // LEVEL 1 TO DO: 
 
+    // -- figure out the card logic 
+    
+    
+    
+    
+    
+    
+    // **** DON'T DELETE 👇 ****
+    
+    // for (const img of images){
+        // this is how to add the images to the dom 
+        // container.append("<img src='./Images/" + img + ".jpg'>").attr("src", `./Images/{img}.jpg`)
+    // }
+    
+    // **** DON'T DELETE 👆 ****
+    
+    
+    
+    
+    
+    
+    // level 2: generate 10 images  with 5 unique ones
+    // level 3:  generate 14 images with 7 unique ones
+    
+    
+    
 }
 
-let combinedArr = imgArr.concat(imgArr2)
-let lenCombArr = 6
-
-let randomizedArr = []
-
-for (const [i,img] of combinedArr.entries()){
-    console.log(i, img, " :i, img")
-    let rando =  Math.floor(Math.random() * lenCombArr)
-
-    container.append("<img src='./Images/" + combinedArr[rando] + ".jpg'>")
-    randomizedArr.push(combinedArr[i])
-
-    combinedArr.splice(rando, 1)
-    lenCombArr--
-    container.append("<img src='./Images/" + combinedArr[rando] + ".jpg'>")
-}
-
-
-
-
-
-console.log(imgArr, imgArr2, combinedArr, randomizedArr, " :imgArr, imgArr2, combinedArr, randomizedArr")
-
-
-
-
-// LEVEL 1 TO DO: 
-// -- add ID to images
-// -- make images display TWICE randomly 
-// -- button to start  playing game
-// -- figure out the card logic 
-
-
-
-
-
-
-// **** DON'T DELETE 👇 ****
-
-// for (const img of images){
-    // this is how to add the images to the dom 
-    // container.append("<img src='./Images/" + img + ".jpg'>").attr("src", `./Images/{img}.jpg`)
-// }
-
-// **** DON'T DELETE 👆 ****
-
-
-
-
-
-
-// level 2: generate 10 images  with 5 unique ones
-// level 3:  generate 14 images with 7 unique ones
-
-
-
+// start game btn 
+// calls game function on click 
+$('#start-game').on('click', () => {
+    $(".level").text(1)
+    game()
+})

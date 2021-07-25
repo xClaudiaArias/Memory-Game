8315372767ea1a89037af5ab21061bb257bdadc1
xClaudiaArias <xclaudiaarias@gmail.com>
2021-07-24 21:56:53 -0400

    fixed random selected images logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,83 +2,35 @@ const images = ["one", "two", "three", "four", "five", "six", "seven"];
 const container = $("#container");
 
 
-// selects random images 
+// selects 3 random images 
 
+let selectedImages = [];
 
-const game = () => {
+let num = images.length
 
-    // get number of images 
-    let numOfImages = images.length; 
-    let imgArr = [];
-    let imgArr2 = [];
-    
-    for (let i = 0; i < 3; i++){
-        let randomIndex = Math.floor(Math.random() * numOfImages)
-        imgArr.push(images[randomIndex])
-        imgArr2.push(images[randomIndex])
-    
-        images.splice(randomIndex, 1)
-        numOfImages -= 1;
-        randomIndex = Math.floor(Math.random() * numOfImages)
+for (let i = 0; i < 3; i++){
+  // picks a random number as index
+    let randomIndex = Math.floor(Math.random() * num)
+
+  // adds element to the list TWICE 
+    for (let j = 0; j < 2; j++){
+        selectedImages.push(images[randomIndex])
+
     }
-    
-    // #randomizes images  
-    let combinedArr = imgArr.concat(imgArr2)
-    let lenCombArr = combinedArr.length
-    let randomizedArr = []
-    
-    for (let i = 0; i < 6; i++){
-        let randomIndex = Math.floor(Math.random() * lenCombArr)
-        console.log(randomIndex, " :randomIndex")
-    
-        randomizedArr.push(combinedArr[randomIndex])
-        combinedArr.splice(randomIndex, 1)
-    
-        lenCombArr -= 1;
-    }
-    
-    // # displays images 
-    for (let [i, img] of randomizedArr.entries()){
-        console.log(i, img, " :i, img")
-        container.append(`<img id="card-${i + 1}" class="card" src="./Images/${img}.jpg">`)
-    }
-    
-    
-    
-    
-    // LEVEL 1 TO DO: 
 
-    // -- figure out the card logic 
-    
-    
-    
-    
-    
-    
-    // **** DON'T DELETE 👇 ****
-    
-    // for (const img of images){
-        // this is how to add the images to the dom 
-        // container.append("<img src='./Images/" + img + ".jpg'>").attr("src", `./Images/{img}.jpg`)
-    // }
-    
-    // **** DON'T DELETE 👆 ****
-    
-    
-    
-    
-    
-    
-    // level 2: generate 10 images  with 5 unique ones
-    // level 3:  generate 14 images with 7 unique ones
-    
-    
-    
+  // removes the element from the list so it can not be chosen again
+    images.splice(randomIndex, 1)
+  // decreases num because the original list is also decreasing 
+    num--
 }
 
-// start game btn 
-// calls game function on click 
-$('#start-game').on('click', () => {
-    $(".level").text(1)
-    game()
-})
+console.log(images, " :images")
+console.log(selectedImages, " :selectedImages")
+
+
+// 1. randomize SELECTED-IMAGES-LIST again to display the elements randomly on the page 
+// FIX THIS 👇👇👇👇👇
+for (let [i, img] of selectedImages.entries()){
+    container.append(`<img id="card-${i + 1}" class="card" src="./Images/${img}.jpg">`)
+}
+

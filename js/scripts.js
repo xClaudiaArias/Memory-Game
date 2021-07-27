51a568a17eb5f0b27f80aea4a18ba8836c029c20
xClaudiaArias <xclaudiaarias@gmail.com>
2021-07-26 22:52:06 -0400

    display randomized images

--- js/scripts.js
+++ js/scripts.js
@@ -5,32 +5,37 @@ const container = $("#container");
 // selects 3 random images 
 
 let selectedImages = [];
-
-let num = images.length
+let num = images.length;
 
 for (let i = 0; i < 3; i++){
   // picks a random number as index
-    let randomIndex = Math.floor(Math.random() * num)
-
+    let randomIndex = Math.floor(Math.random() * num);
   // adds element to the list TWICE 
     for (let j = 0; j < 2; j++){
-        selectedImages.push(images[randomIndex])
-
+      selectedImages.push(images[randomIndex]);
     }
-
   // removes the element from the list so it can not be chosen again
-    images.splice(randomIndex, 1)
+    images.splice(randomIndex, 1);
   // decreases num because the original list is also decreasing 
-    num--
+    num--;
 }
 
-console.log(images, " :images")
-console.log(selectedImages, " :selectedImages")
+// randomize images
+let randomizedImages = [];
+let lenSelected = selectedImages.length
 
+for (let i = 0; i < 6; i++){
+  let randomIndex = Math.floor(Math.random() * lenSelected);
 
-// 1. randomize SELECTED-IMAGES-LIST again to display the elements randomly on the page 
-// FIX THIS 👇👇👇👇👇
-for (let [i, img] of selectedImages.entries()){
-    container.append(`<img id="card-${i + 1}" class="card" src="./Images/${img}.jpg">`)
+  randomizedImages.push(selectedImages[randomIndex]);
+  selectedImages.splice(randomIndex, 1)
+  lenSelected--;
 }
 
+// display random images on screen 
+for (let [i, img] of randomizedImages.entries()){
+  container.append(`<img id="card-${i + 1}" class="card" src="./Images/${img}.jpg">`)
+}
+
+
+// TO DO: WORK ON CARD LOGIC 

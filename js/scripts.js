98e48327383311de7fbdf992e809771de422d3c7
claudia <xclaudiaarias@gmail.com>
2021-11-07 20:14:54 -0500

    level 2 works. hoowever it returns an undefined image error.

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,133 @@ let level = parseInt(level_container.innerHTML);
 let score = parseInt(score_container.innerHTML);
 
 let firstLev = 6
+let rand = 3
+let randomizedImages;
+
+
+const loadCards = (lev, ra) => {
+  for (let i = 0; i < lev; i++){
+      // parent element 
+      let cardSpace = document.createElement("div")
+      container.append(cardSpace)
+  
+      // child element 
+      let newCard = document.createElement("div")
+      cardSpace.append(newCard)
+  }
+  $( "#container div" ).addClass("card-space");
+  $( ".card-space div" ).addClass("card");
+
+  $(".card").each(function(i, e){
+
+    let backDiv = document.createElement("div")
+    let frontDiv = document.createElement("div")
+
+    backDiv.classList.add("back-div")
+    frontDiv.classList.add("front-div")
+
+    e.append(backDiv)
+    e.append(frontDiv)
+
+  })
+  
+  $(".card").removeClass("card-space");
+
+  // add card__side class 
+  $(".front-div").addClass("card__side")
+  $(".back-div").addClass("card__side")
+
+
+  let selectedImages = [];
+  let num = images.length;
+
+  for (let i = 0; i < ra; i++){
+    // picks a random number as index
+      let randomIndex = Math.floor(Math.random() * num);
+    // adds element to the list TWICE 
+      for (let j = 0; j < 2; j++){
+        selectedImages.push(images[randomIndex]);
+      }
+    // removes the element from the list so it can not be chosen again
+      images.splice(randomIndex, 1);
+    // decreases num because the original list is also decreasing 
+      num--;
+  }
+
+  randomizedImages = [];
+  let lenSelected = selectedImages.length
+
+  for (let i = 0; i < lev; i++){
+    let randomIndex = Math.floor(Math.random() * lenSelected);
+
+    randomizedImages.push(selectedImages[randomIndex]);
+    selectedImages.splice(randomIndex, 1)
+    lenSelected--;
+  }
+
+  $(".front-div").each(function(i, e){
+
+    let newImg = document.createElement('img')
+    newImg.setAttribute("src",`./Images/${randomizedImages[i]}.jpg`)
+    newImg.classList.add("front-img")
+    newImg.setAttribute("id", `${randomizedImages[i]}`)
+
+    e.append(newImg)
+  })
+
+
+  let matched = []
+
+  const matchCards = () => {
+
+    $(".card").on('click', function(e){
+      if (matched.length < 2) {
+        matched.push(this)
+        $(this).toggleClass("is-flipped");
+      } 
+    })
+
+
+    return matched
+
+  }
+
+  matchCards()
+
+  // -------- COMPARING THE CARDS 
+
+  const compare = (arr) => {
+    if (arr[0].lastChild.children[0].id === arr[1].lastChild.children[0].id && matched.length == 2) {
+      score++;
+      score_container.innerHTML = score + ""
+      $(arr[0]).addClass("is-flipped")
+      $(arr[1]).addClass("is-flipped")
+      matched = []
+    } else {
+      setTimeout(function(){
+        $(arr[0]).toggleClass("is-flipped")
+        $(arr[1]).toggleClass("is-flipped")
+      },1000)
+      matched = []
+    }
+  }
+
+  // -------- REFRESHING THE PAGE TO GET THE MATCHED CARDS RETURNED
+  let checkMatched = setInterval(function(){
+    if (matched.length == 2) {
+      compare(matched)
+    }
+  }, 500)
+}
+
 
 const game = () => {
 
   $("#container").css('visibility', 'visible');
   $("#level").css('visibility', 'visible');
   $("#score").css('visibility', 'visible');
+
+  // ----------------------------------------------------------------------------------------------------------------------------------
 
   for (let i = 0; i < firstLev; i++){
     // parent element 
@@ -50,7 +171,7 @@ const game = () => {
   let selectedImages = [];
   let num = images.length;
 
-  for (let i = 0; i < 3; i++){
+  for (let i = 0; i < rand; i++){
     // picks a random number as index
       let randomIndex = Math.floor(Math.random() * num);
     // adds element to the list TWICE 
@@ -66,7 +187,7 @@ const game = () => {
   let randomizedImages = [];
   let lenSelected = selectedImages.length
 
-  for (let i = 0; i < 6; i++){
+  for (let i = 0; i < firstLev; i++){
     let randomIndex = Math.floor(Math.random() * lenSelected);
 
     randomizedImages.push(selectedImages[randomIndex]);
@@ -83,6 +204,8 @@ const game = () => {
 
     e.append(newImg)
   })
+
+  // ----------------------------------------------------------------------------------------------------------------------------------
 
   let matched = []
 
@@ -106,7 +229,6 @@ const game = () => {
 
   const compare = (arr) => {
     if (arr[0].lastChild.children[0].id === arr[1].lastChild.children[0].id && matched.length == 2) {
-      console.log( "SAME, YOU WIN")
       score++;
       score_container.innerHTML = score + ""
       $(arr[0]).addClass("is-flipped")
@@ -135,40 +257,42 @@ const game = () => {
 
   check_score = setInterval(function(){
 // ---------
+  console.log(randomizedImages, ' firstttttt')
     if (score === 3){
       console.log("YES score is " + score)
-      level += 1
+      level = 2
       level_container.innerHTML = level
+      firstLev = 10
+      rand = 5
+      console.log('level is ' +  level)
+      randomizedImages = []
+      // matched = []
+      setTimeout(function() { 
+        $("#container").empty();
+        loadCards(firstLev, rand)
+      }, 2000);
       clearInterval(check_score)
+      console.log(randomizedImages, ' second')
+      console.log(firstLev, rand, ' :firstlev rand')
+      // clearInterval(check_score)
     } 
+
   }, 500)
+
+  return check_score
+
+
 }
-
-
-
+// kkw
 // START GAME BUTTON 👇
 
 let start_btn = document.querySelector('#start-game');
 
 $(start_btn).on('click', function(){
-  game(level, score)
+  game()
+  $(this).prop("disabled",true);
 })
 
 
-// work on leveling up 
 
-// const levelUp = setInterval(function(){
-//   if (score === 3){
-//     console.log("LEVEL IS 22222")
-//     firstLev = 10
-//     clearInterval(levelUp)
-//   } else if (score === 5) {
-//     console.log("LEVEL IS 33333")
-//     firstLev = 14  
-//     clearInterval(levelUp)
-//   } else if (score ===)
-
-
-//   console.log(firstLev, ' i am first lev')
-// }, 500)
 
